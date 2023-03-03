80d7384c76533ce6e2907a7e795d4fe166ce4061
Drew Herron <herron@mailbox.org>
2023-03-02 21:38:09 -0800

    Just notes to myself

--- imp-mode/imp-mode.el
+++ imp-mode/imp-mode.el
@@ -20,7 +20,46 @@
 ;;| S | (move-beginning-of-line) | (scroll-up-command) | (scroll-down-command) | (move-end-of-line) |
 ;;| M | (backward-sentence)      | (forward-paragraph) | (backward-paragraph)  | (forward-sentence) |
 ;;| C | (windmove-left)          | (next-buffer)       | (previous-buffer)     | (windmove-right)   |
- 
+
+
+Note to myself:
+It may be annoying to go in and out of this mode all the time.
+You may want to add selection and movement, copy paste, etc,
+maybe just the emacs commands without ctrl, or.... something
+Just an idea
+What would those be?
+space: (set-mark-command) or (push-mark)
+w
+y
+k (in Colemak)
+Compare god mode keybindings for more..
+WAIT
+Think about C-d and M-d
+So, keep Ctrl and Meta,
+I'm using mnei for movement
+Ctrl/Meta e doesn't matter because I'm rebinding it in that same space.
+What else is there?
+C-m RET - worthless
+M-m back-to-indentation - first non-whitespace char on line
+C-M-m undefined
+C-n next-line               no prob 
+M-n undefined               no probM
+C-M-n forward-list
+C-e move-end-of-line        no prob
+M-e forward-sentence        no prob
+C-M-e end-of-defun
+C-i indent-for-tab          
+M-i tab-to-tab-stop - should be... on TAB?
+C-M-i completion-at-point
+
+hmmmm
+
+Woah what about... don't have an imp-mode???
+Instead of a mode, keep mods (more like emacs)
+Can't have shift though.
+Ctrl    left down up right 
+Meta    sentences and regions
+C+M     windows and buffers 
 
 ;;; Code:
 
